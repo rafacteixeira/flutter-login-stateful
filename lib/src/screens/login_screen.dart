7eb01d9e1d9a14ee282edc445e00d26309a68670
Rafael Cruz <rafacteixeira@gmail.com>
2018-10-21 23:41:03 -0300

    form validation

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -8,11 +8,13 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
 
   Widget build(context) {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Form(
+        key: formKey,
         child: Column(
           children: <Widget>[
             emailField(),
@@ -29,9 +31,10 @@ class LoginScreenState extends State<LoginScreen> {
     return TextFormField(
       decoration: InputDecoration(
         labelText: "Email Address",
-        hintText: "you@example.com"
+        hintText: "you@example.com",
       ),
       keyboardType: TextInputType.emailAddress,
+      validator: validateEmailField,
     );
   }
 
@@ -39,21 +42,36 @@ class LoginScreenState extends State<LoginScreen> {
     return TextFormField(
       decoration: InputDecoration(
         labelText: "Password",
-        hintText: "type your password here"
+        hintText: "type your password here",
       ),
       obscureText: false,
+      validator: validatePasswordField,
     );
   }
 
   Widget submitButton() {
     return RaisedButton(
-      color: Colors.red,
+      color: Colors.blue,
       child: Text("Submit"),
       onPressed: submitFunction,
     );
   }
 
   void submitFunction() {
+    formKey.currentState.validate();
+  }
 
+  String validateEmailField(String value) {
+    if (!value.contains('@')) {
+      return 'please enter a valid email';
+    }
+    return null;
+  }
+
+  String validatePasswordField(String value) {
+    if (value.length < 4) {
+      return 'password must be at least 4 characters';
+    }
+    return null;
   }
 }
